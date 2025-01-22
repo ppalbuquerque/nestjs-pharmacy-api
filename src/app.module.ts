5a9cf5b40baf398a002cf31e95d6d0110b5169dd
pedro.albuquerque <albpedro458@gmail.com>
2025-01-22 16:14:00 -0300

    add new files module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MedicationModule } from './medication/medication.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [MedicationModule, PrismaModule],
+  imports: [MedicationModule, PrismaModule, FilesModule],
   controllers: [],
   providers: [],
 })
