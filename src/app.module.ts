ecdfcc5effcc841c9239930f6f35a4414a0cedf0
pppalbuquerque <albpedro458@gmail.com>
2025-10-15 16:26:31 -0300

    add db connection params from environment files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,23 +6,28 @@ import { Medication } from './medication/medication.entitity';
 import { MedicationSubscriber } from './medication/medication.subscriber';
 import { File } from './files/entities/file.entity';
 import { FilesModule } from './files/files.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MedicationModule,
     FilesModule,
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'pharma',
-      entities: [Medication, File],
-      subscribers: [MedicationSubscriber],
-      synchronize: false,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT') || ''),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Medication, File],
+        subscribers: [MedicationSubscriber],
+        synchronize: false,
+        ssl: true,
+      }),
     }),
   ],
   controllers: [],
