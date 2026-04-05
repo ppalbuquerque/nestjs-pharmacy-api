b80d9f6043c03cb753c649db2f7f3a9afb50def7
pppalbuquerque <albpedro458@gmail.com>
2026-04-05 08:42:43 -0300

    chore(medication): adicionar validadores class-validator nos DTOs de criação e atualização

--- src/medication/dto/create-medication.dto.ts
+++ src/medication/dto/create-medication.dto.ts
@@ -1,30 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateMedicationDto {
   @ApiProperty()
+  @IsString()
   name: string;
 
   @ApiProperty()
+  @IsString()
   chemicalComposition: string;
 
   @ApiProperty()
+  @IsNumber()
   stockAvailability: number;
 
   @ApiProperty()
+  @IsString()
   shelfLocation: string;
 
   @ApiProperty()
+  @IsNumber()
   boxPrice: number;
 
   @ApiProperty()
+  @IsNumber()
   unitPrice: number;
 
   @ApiProperty()
+  @IsString()
   usefulness: string;
 
   @ApiProperty()
+  @IsString()
   dosageInstructions: string;
 
   @ApiProperty()
+  @IsString()
   samplePhotoUrl: string;
 }
--- src/medication/dto/update-medication.dto.ts
+++ src/medication/dto/update-medication.dto.ts
@@ -1,33 +1,57 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class UpdateMedicationDTO {
   @ApiProperty()
+  @IsNumber()
   id: number;
 
   @ApiProperty()
+  @IsOptional()
+  @IsString()
   name?: string;
 
   @ApiProperty()
+  @IsOptional()
+  @IsString()
   chemicalComposition?: string;
 
   @ApiProperty()
+  @IsOptional()
+  @IsNumber()
   stockAvailability?: number;
 
   @ApiProperty()
+  @IsOptional()
+  @IsString()
   shelfLocation?: string;
 
   @ApiProperty()
+  @IsOptional()
+  @IsNumber()
   boxPrice?: number;
 
   @ApiProperty()
+  @IsOptional()
+  @IsNumber()
   unitPrice?: number;
 
   @ApiProperty()
+  @IsOptional()
+  @IsString()
   usefulness?: string;
 
   @ApiProperty()
+  @IsOptional()
+  @IsString()
   dosageInstructions?: string;
 
   @ApiProperty()
+  @IsOptional()
+  @IsString()
   samplePhotoUrl?: string;
 }
